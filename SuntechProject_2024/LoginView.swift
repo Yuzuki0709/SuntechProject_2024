f694ed39d9b0061f57ba9d0d95b0e49257b7923c
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-30 22:48:56 +0900

    add: LoginView.swiftを追加

---
+++ SuntechProject_2024/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/06/30.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
