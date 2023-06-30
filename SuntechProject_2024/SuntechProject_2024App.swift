2408f68151777146f3d1cbb73312742d735faafd
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-30 22:46:52 +0900

    First commit

---
+++ SuntechProject_2024/SuntechProject_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SuntechProject_2024App.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct SuntechProject_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuntechProject_2024UITests/SuntechProject_2024UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SuntechProject_2024UITestsLaunchTests.swift
+//  SuntechProject_2024UITests
+//
+//  Created by 岩本竜斗 on 2023/06/30.
+//
+
+import XCTest
+
+final class SuntechProject_2024UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
