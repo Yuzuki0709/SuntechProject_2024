86d089d8ad9f1f1ac0988ec46efb7b13f0790ce4
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-03 22:21:47 +0900

    add: TimetableView.swiftを追加

---
+++ SuntechProject_2024/UI/TimetableView.swift
@@ -0,0 +1,22 @@
+//
+//  TimetableView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/03.
+//
+
+import SwiftUI
+
+struct TimetableView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct TimetableView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimetableView()
+    }
+}
