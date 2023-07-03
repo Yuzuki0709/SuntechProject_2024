df12eb15b9e862aba89c4e9c765a37a5b1712045
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-03 13:39:03 +0900

    add: LoginViewModel.swiftを追加

---
+++ SuntechProject_2024/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/03.
+//
+
+import Foundation
+import SwiftUI
+
+final class LoginViewModel: ObservableObject {
+    
+}
