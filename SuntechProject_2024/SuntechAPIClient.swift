6db2e88e0e0849618342b2496ae1590d2ae76080
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-03 13:45:26 +0900

    add: SuntechAPIClent.swiftを追加

---
+++ SuntechProject_2024/SuntechAPIClient.swift
@@ -0,0 +1,16 @@
+//
+//  SuntechAPIClient.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/03.
+//
+
+import Foundation
+
+public protocol SuntechAPIClientProtocol {
+    
+}
+
+final class SuntechAPIClient {
+    
+}
