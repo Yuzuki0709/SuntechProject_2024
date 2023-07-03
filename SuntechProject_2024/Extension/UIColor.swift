8d6a7964fc6fd599b17bccb7c81d4ceb16877766
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-03 22:35:59 +0900

    add: UIColor.swiftを追加

---
+++ SuntechProject_2024/Extension/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/03.
+//
+
+import SwiftUI
+
+extension UIColor {
+    static let mainColor = UIColor(Color.mainColor)
+}
+
