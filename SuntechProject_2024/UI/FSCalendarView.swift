fa454c48fe7405e23bc0e5fb372e01edc3b62575
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-03 22:25:57 +0900

    add: FSCalendarView.swiftを追加

---
+++ SuntechProject_2024/UI/FSCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  FSCalendarView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/03.
+//
+
+import UIKit
+
+class FSCalendarView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
