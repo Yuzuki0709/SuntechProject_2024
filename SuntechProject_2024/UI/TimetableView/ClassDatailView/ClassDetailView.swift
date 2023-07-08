6666159c8aa707cc06f33df831d3869ed6b178ea
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-08 23:30:08 +0900

    add: ClassDetailView.swiftを追加

---
+++ SuntechProject_2024/UI/TimetableView/ClassDatailView/ClassDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ClassDetailView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/08.
+//
+
+import SwiftUI
+
+struct ClassDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClassDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClassDetailView()
+    }
+}
