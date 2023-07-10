a45504cf52655393c9f7211bac3f83b259d1cf76
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-10 16:16:31 +0900

    add: AttendanceLog.swiftを追加

---
+++ SuntechProject_2024/Model/AttendanceStatus/AttendanceLog.swift
@@ -0,0 +1,13 @@
+//
+//  AttendanceLog.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/10.
+//
+
+import Foundation
+
+struct AttendanceLog {
+    let date: Date
+    let status: AttendanceStatus
+}
