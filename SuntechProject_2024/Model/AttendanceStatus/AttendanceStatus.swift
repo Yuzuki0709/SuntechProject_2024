0e05495e8a4a3c171d646802517c7ffc5714e4f0
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-10 16:08:42 +0900

    add: AttendanceStatus.swiftを追加

---
+++ SuntechProject_2024/Model/AttendanceStatus/AttendanceStatus.swift
@@ -0,0 +1,14 @@
+//
+//  AttendanceStatus.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/10.
+//
+
+import Foundation
+
+enum AttendanceStatus: String {
+    case attendance = "出席"
+    case absence = "欠席"
+    case lateness = "遅刻"
+}
