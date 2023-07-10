7feea7761db0e6707fe2868455b8d699a3406c71
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-10 16:18:17 +0900

    add: ClassAttendance.swiftを追加

---
+++ SuntechProject_2024/Model/AttendanceStatus/ClassAttendance.swift
@@ -0,0 +1,13 @@
+//
+//  ClassAttendance.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/10.
+//
+
+import Foundation
+
+struct ClassAttendance {
+    let classId: String
+    let logs: [AttendanceLog] = []
+}
