94385540851f859105233115afeb4daaf73cd330
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-11 20:28:06 +0900

    add: AttendanceRealmManager.swiftを追加

---
+++ SuntechProject_2024/Model/AttendanceRealmManager.swift
@@ -0,0 +1,30 @@
+//
+//  AttendanceRealmManager.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/11.
+//
+
+import Foundation
+import RealmSwift
+
+final class AttendanceRealmManager {
+    static let shared = AttendanceRealmManager()
+    
+    private var localRealm: Realm?
+    
+    init() {
+        openRealm()
+    }
+    
+    private func openRealm() {
+        do {
+            let config = Realm.Configuration(schemaVersion: 1)
+            Realm.Configuration.defaultConfiguration = config
+            
+            localRealm = try Realm()
+        } catch {
+            print("Error opening Realm: \(error)")
+        }
+    }
+}
