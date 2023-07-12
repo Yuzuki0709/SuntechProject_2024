f239be8abf94eaa59c89ca44f62659f144201b58
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-12 09:39:51 +0900

    add: AttendanceStatusViewModel.swiftを追加

---
+++ SuntechProject_2024/UI/TimetableView/ClassDatailView/AttendanceStatusViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  AttendanceStatusViewModel.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/12.
+//
+
+import SwiftUI
+import Foundation
+
+final class AttendanceStatusViewModel: ObservableObject {
+    private let classData: Class
+    private let realmManager: AttendanceRealmManager
+    
+    var attendanceCount: Int {
+        guard let classAttendance = realmManager.getClassAttendance(classId: classData.id) else { return 0 }
+        return classAttendance.logs.filter { $0.status == .attendance }.count
+    }
+    
+    var absenceCount: Int {
+        guard let classAttendance = realmManager.getClassAttendance(classId: classData.id) else { return 0 }
+        
+        return classAttendance.logs.filter { $0.status == .absence }.count
+    }
+    
+    var latenessCount: Int {
+        guard let classAttendance = realmManager.getClassAttendance(classId: classData.id) else { return 0 }
+        
+        return classAttendance.logs.filter { $0.status == .lateness }.count
+    }
+    
+    init(classData: Class, realmManager: AttendanceRealmManager = AttendanceRealmManager()) {
+        self.classData = classData
+        self.realmManager = realmManager
+    }
+}
+
