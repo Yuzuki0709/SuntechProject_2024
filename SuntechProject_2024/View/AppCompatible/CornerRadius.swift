150a5d7d6b3b3679820050b001dd5a1605d6162f
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-18 20:47:26 +0900

    add: CornerRadius.swiftを追加

---
+++ SuntechProject_2024/View/AppCompatible/CornerRadius.swift
@@ -0,0 +1,17 @@
+//
+//  CornerRadius.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/18.
+//
+
+import SwiftUI
+
+public extension AppStyle {
+    struct CornerRadius {
+        public let radiusXS: CGFloat = 5
+        public let radiusS: CGFloat = 8
+        public let radiusM: CGFloat = 16
+        public let radiusL: CGFloat = 24
+    }
+}
