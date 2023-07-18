28d545aebed469ae4d59d86950289e7bcc768fd9
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-18 20:51:46 +0900

    SpaceLayout.swiftを追加

---
+++ SuntechProject_2024/View/AppCompatible/SpaceLayout.swift
@@ -0,0 +1,29 @@
+//
+//  SpaceLayout.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/18.
+//
+
+import SwiftUI
+
+public extension AppStyle {
+    struct SpaceLayout {
+        /// Default value: 4
+        public let spacingXXS: CGFloat = 4
+        /// Default value: 8
+        public let spacingXS: CGFloat = 8
+        /// Default value: 16
+        public let spacingS: CGFloat = 16
+        /// Default value: 24
+        public let spacingM: CGFloat = 24
+        /// Default value: 32
+        public let spacingL: CGFloat = 32
+        /// Default value: 40
+        public let spacingXL: CGFloat = 40
+        /// Default value: 48
+        public let spacingXXL: CGFloat = 48
+        /// Default value: 64
+        public let spacingXXXL: CGFloat = 64
+    }
+}
