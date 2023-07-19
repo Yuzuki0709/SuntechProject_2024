4206f6e5d4c61a3721cc57ccd5470364118356e0
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-19 22:44:52 +0900

    add: WebView.swiftを追加

---
+++ SuntechProject_2024/View/WebView/WebView.swift
@@ -0,0 +1,22 @@
+//
+//  WebView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/19.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
