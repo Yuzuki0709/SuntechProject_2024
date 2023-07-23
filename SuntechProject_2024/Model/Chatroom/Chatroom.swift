77215a93930312e727f86bbac98c42602b18f8ee
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-23 22:05:51 +0900

    add: Chatroom.swiftを追加

---
+++ SuntechProject_2024/Model/Chatroom/Chatroom.swift
@@ -0,0 +1,15 @@
+//
+//  Chatroom.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/23.
+//
+
+import Foundation
+
+public struct Chatroom: Codable {
+    public let id: Int64
+    public let name: String
+    public let createdAt: Date
+    public let updateAt: Date
+}
