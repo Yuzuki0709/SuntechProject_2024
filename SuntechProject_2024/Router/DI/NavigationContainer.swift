429e6d97c42871b165ad5f24583b7f42a2d8e059
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-29 19:02:58 +0900

    add: NavigationContainer.swiftを追加

---
+++ SuntechProject_2024/Router/DI/NavigationContainer.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationContainer.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/29.
+//
+
+import Foundation
+import Factory
+
+final class NavigationContainer: SharedContainer {
+    static var shared = NavigationContainer()
+    var manager = ContainerManager()
+    
+    
+}
