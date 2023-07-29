01b9acd3379a238c450a11e043fd10a2b92f457f
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-29 10:26:08 +0900

    add: TimetableFlowController.swiftをついか

---
+++ SuntechProject_2024/Feature/TimetableView/TimetableFlowController.swift
@@ -0,0 +1,14 @@
+//
+//  TimetableFlowController.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/29.
+//
+
+import UIKit
+import Combine
+
+final class TimetableFlowController: HostingController<TimetableView>, TimetableFlowControllerService {
+    func start() {
+    }
+}
---
+++ SuntechProject_2024/Router/Feature/Timetable/TimetableFlowControllerService.swift
@@ -0,0 +1,10 @@
+//
+//  TimetableFlowControllerService.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/29.
+//
+
+import Foundation
+
+public protocol TimetableFlowControllerService: FlowController {}
