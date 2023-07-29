36258e1389bc1aad52d217b303e0c4723a18e134
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-07-29 18:11:10 +0900

    add: AttendanceStatusFlowController.swiftを追加

---
+++ SuntechProject_2024/Feature/ClassDetailView/AttendanceStatusView/AttendanceStatusFlowController.swift
@@ -0,0 +1,14 @@
+//
+//  AttendanceStatusFlowController.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/07/29.
+//
+
+import Foundation
+import Combine
+
+final class AttendanceStatusFlowController: HostingController<AttendanceStatusView>, AttendanceStatusFlowControllerService {
+    func start() {
+    }
+}
