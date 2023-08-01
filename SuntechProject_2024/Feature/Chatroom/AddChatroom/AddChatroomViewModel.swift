635ad9f7c529ff64cdfa2f7c45b60fd963033a81
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-08-01 23:51:20 +0900

    add:  AddChatroomViewModel.swiftを追加

---
+++ SuntechProject_2024/Feature/Chatroom/AddChatroom/AddChatroomViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddChatroomViewModel.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/08/01.
+//
+
+import Foundation
+import Combine
+
+final class AddChatroomViewModel: ObservableObject {
+    
+}
