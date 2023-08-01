e53d3ec7000f1728951dc3d33d145768ba01ab2c
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-08-01 23:48:40 +0900

    add: AddChatroomView.swiftを追加

---
+++ SuntechProject_2024/Feature/Chatroom/AddChatroom/AddChatroomView.swift
@@ -0,0 +1,20 @@
+//
+//  AddChatroomView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/08/01.
+//
+
+import SwiftUI
+
+struct AddChatroomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddChatroomView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddChatroomView()
+    }
+}
