cf9fbf4900265161fbe5528e19a782e27ac7308b
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-08-01 23:56:18 +0900

    add: AddChatroomFlowController.swiftを追加

---
+++ SuntechProject_2024/Feature/Chatroom/AddChatroom/AddChatroomFlowController.swift
@@ -0,0 +1,13 @@
+//
+//  AddChatroomFlowController.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/08/01.
+//
+
+import Foundation
+
+final class AddChatroomFlowController: HostingController<AddChatroomView>, AddChatroomFlowControllerService {
+    func start() {
+    }
+}
---
+++ SuntechProject_2024/Router/Feature/Chatroom/AddChatroom/AddChatroomFlowControllerService.swift
@@ -0,0 +1,10 @@
+//
+//  AddChatroomFlowControllerService.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/08/01.
+//
+
+import Foundation
+
+public protocol AddChatroomFlowControllerService: FlowController {}
