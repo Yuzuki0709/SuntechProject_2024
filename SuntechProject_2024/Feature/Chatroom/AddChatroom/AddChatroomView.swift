65a8a1a0ae719297c516d3c059864fe6db0342d6
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-08-03 18:15:46 +0900

    feat: 教師と生徒で分けて表示するようにした

--- SuntechProject_2024/Feature/Chatroom/AddChatroom/AddChatroomView.swift
+++ SuntechProject_2024/Feature/Chatroom/AddChatroom/AddChatroomView.swift
@@ -16,11 +16,9 @@ struct AddChatroomView: View {
     
     var body: some View {
         List {
-            ForEach(viewModel.chatUsers) { user in
-                chatUserListRow(user)
-            }
+            userSections
         }
-        .listStyle(.plain)
+        .listStyle(.grouped)
         .loading(viewModel.isLoading)
         .onAppear {
             viewModel.fetchAllChatUser()
@@ -30,8 +28,9 @@ struct AddChatroomView: View {
     private func chatUserListRow(_ user: ChatUser) -> some View {
         HStack(spacing: .app.space.spacingS) {
             Image(systemName: "person")
+                .scaledToFit()
+                .frame(width: 35, height: 35)
                 .foregroundColor(.white)
-                .padding()
                 .background(Color.gray)
                 .clipShape(Circle())
             
@@ -45,6 +44,34 @@ struct AddChatroomView: View {
             
         }
         .listRowBackground(Color.clear)
+    }
+    
+    private var userSections: some View {
+        Group {
+            Section("教師") {
+                ForEach(viewModel.chatUsers.filter { $0.type == .teacher }) { user in
+                    chatUserListRow(user)
+                }
+            }
+            
+            Section("コンピュータ・コミュニケーション科") {
+                ForEach(viewModel.chatUsers.filter { $0.type == .student(.C) }) { user in
+                    chatUserListRow(user)
+                }
+            }
+            
+            Section("情報システム科") {
+                ForEach(viewModel.chatUsers.filter { $0.type == .student(.S) }) { user in
+                    chatUserListRow(user)
+                }
+            }
+            
+            Section("マルチメディア科") {
+                ForEach(viewModel.chatUsers.filter { $0.type == .student(.M) }) { user in
+                    chatUserListRow(user)
+                }
+            }
+        }
     }
 }
 
