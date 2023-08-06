1b4b52776fc0c94635458b3c0e89dda710e1715d
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-08-06 23:45:20 +0900

    add: ChatMessageView.swiftを追加

---
+++ SuntechProject_2024/Feature/Chatroom/ChatMessage/ChatMessageView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatMessageView.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/08/06.
+//
+
+import SwiftUI
+
+struct ChatMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatMessageView()
+    }
+}
