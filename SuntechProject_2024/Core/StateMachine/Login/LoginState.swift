9f4fb91d79931790b4acb8eedb22d4b270aafb02
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-10-01 22:35:51 +0900

    add: LoginState.swiftを追加

---
+++ SuntechProject_2024/Core/StateMachine/Login/LoginState.swift
@@ -0,0 +1,15 @@
+//
+//  LoginState.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/10/01.
+//
+
+import Foundation
+
+enum LoginState: Equatable {
+    case initial
+    case loading
+    case results
+    case error
+}
