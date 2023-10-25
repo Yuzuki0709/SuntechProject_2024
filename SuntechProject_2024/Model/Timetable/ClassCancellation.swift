244151d109cf92eefa0b1fb81783ac5562a29e79
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-10-25 19:16:02 +0900

    add: ClassCancellation.swiftを追加

---
+++ SuntechProject_2024/Model/Timetable/ClassCancellation.swift
@@ -0,0 +1,14 @@
+//
+//  ClassCancellation.swift
+//  SuntechProject_2024
+//
+//  Created by 岩本竜斗 on 2023/10/25.
+//
+
+import Foundation
+
+public struct ClassCancellation: Identifiable, Codable {
+    public var id: UUID = .init()
+    public let classId: String
+    public let date: Date
+}
